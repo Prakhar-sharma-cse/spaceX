341fc6a4636432f349d5202b261992f29c4637c1
Prakhar Sharma <62322737+Prakhar-sharma-cse@users.noreply.github.com>
2021-08-08 08:46:45 +0530

    code uploaded from local storage to github

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// (function () {
+//   var socials = document.querySelectorAll('.social  div')
+
+//   socials.forEach(function(sociale,index) {
+//       sociale.style.animation = `movein 12s ease-in-out forwards ${index/7 + 0.2}s`
+//       })
+
+//   let rocketPlaces = document.querySelectorAll(".rocket-body span");
+
+//   let rocket = document.querySelector(".rocket");
+
+//   let animaionstart = window.innerHeight / 5;
+
+//   let rocketoffsetTop = rocket.offsetTop;
+
+//   document.addEventListener("scroll", (e) => {
+//     if (window.scrollY > (rocketoffsetTop - animaionstart)) {
+//       rocketPlaces[0].classList.add("active");
+//       rocketPlaces[1].classList.add("active");
+//     }
+//   })
+// }());
+
+
